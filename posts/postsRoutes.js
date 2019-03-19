84c49f848397224dd02a30b0852f9b01cc2215ea
UnknownMonk <smodcartoon@gmail.com>
2019-03-19 15:40:34 -0400

    finished get route and get:id route and post

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const db = require('../data/db');
 
-server.use(express.json());
+router.use(express.json());
 
 router.post('/', (req, res) => {
+  const { title, contents } = req.body;
   if (!title || !contents) {
     res.status(400).json({
       errorMessage: 'Please provide title and contents for the post.'
@@ -19,6 +20,38 @@ router.post('/', (req, res) => {
       res.status(500).json({
         error: 'There was an error while saving the post to the database'
       });
+    });
+});
+
+router.get('/', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json({ 'Here are your posts': posts });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+
+    .then(posts => {
+      if (id) {
+        res.status(200).json({ 'Here are your posts': posts });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res
+        .status()
+        .json({ error: 'The posts information could not be retrieved.' });
     });
 });
 
